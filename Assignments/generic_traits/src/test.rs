6412f08190197e642600e2de4bb7c2ee1d836703
Sandeep-Knoldus <88487228+Sandeep-Knoldus@users.noreply.github.com>
2021-10-26 23:09:35 +0530

    Update test.rs

--- Assignments/generic_traits/src/test.rs
+++ Assignments/generic_traits/src/test.rs
@@ -9,6 +9,14 @@ pub fn compare_success() {
 pub fn compare_successfully() {
     assert_eq!(compare_numbers(1, 100), 1);
 }
+#[test]
+pub fn min_success() {
+    assert_eq!(compare_numbers(33, -1), -1);
+}
+#[test]
+pub fn min_successfully() {
+    assert_eq!(compare_numbers(-9, -8), -9);
+}
 
 /// Sorting Generic Numbers
 pub use crate::question1::sorting_generic::sorting_numbers;
@@ -26,31 +34,38 @@ pub fn sorting_successfully() {
         vec![1.3, 2.7, 5.1, 7.4, 8.6, 9.1]
     );
 }
+#[test]
+pub fn sorting_pass() {
+    assert_eq!(
+        sorting_numbers(&mut vec![5, -1, -2, 8, 2, 9]),
+        vec![-2, -1, 2, 5, 8, 9]
+    );
+}
 
 /// GP Generic
 pub use crate::question2::gp_generic::GeometricSeries;
 pub use crate::question2::gp_generic::Iterator;
 #[test]
 pub fn gp_success() {
-    let values = GeometricSeries {
+    let mut values = GeometricSeries {
         first_number: 2,
         ratio: 2,
-        length: 11,
+        current_number: 1,
     };
     assert_eq!(
-        GeometricSeries::generate(values.first_number, values.ratio, values.length),
+        values.take(11),
         [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]
     );
 }
 #[test]
 pub fn gp_successfully() {
-    let values = GeometricSeries {
+    let mut values = GeometricSeries {
         first_number: 1,
         ratio: 2,
-        length: 11,
+        current_number: 1,
     };
     assert_eq!(
-        GeometricSeries::generate(values.first_number, values.ratio, values.length),
+        values.take(11),
         [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
     );
 }
