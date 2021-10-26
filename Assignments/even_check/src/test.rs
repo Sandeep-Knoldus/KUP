8f7f64db58e84bf577f69a3abf73c9cec9484246
Sandeep-Knoldus <88487228+Sandeep-Knoldus@users.noreply.github.com>
2021-10-26 13:46:19 +0530

    Update test.rs

--- Assignments/even_check/src/test.rs
+++ Assignments/even_check/src/test.rs
@@ -1,21 +1,41 @@
 #[cfg(test)]
-use crate::question1::even_check::even_check;
+use crate::question1::even_check::check;
 #[test]
-pub fn check_no_test1() {
-    assert_eq!(even_check(10), Ok("EVEN".to_string()));
+pub fn check_no_ok() {
+    assert_eq!(check(10), Ok("EVEN".to_string()));
 }
 
 #[test]
-pub fn check_no_test2() {
-    assert_eq!(even_check(13), Err("Provide correct number".to_string()));
+pub fn check_no_err() {
+    assert_eq!(check(13), Err("Provide correct number".to_string()));
 }
 
 #[test]
-pub fn check_no_test3() {
-    assert_eq!(even_check(8), Ok("EVEN".to_string()));
+pub fn check_number_ok() {
+    assert_eq!(check(8), Ok("EVEN".to_string()));
 }
 
 #[test]
-pub fn check_no_test4() {
-    assert_eq!(even_check(1), Err("Provide correct number".to_string()));
+pub fn check_number_err() {
+    assert_eq!(check(1), Err("Provide correct number".to_string()));
+}
+
+#[test]
+pub fn number_ok() {
+    assert_eq!(check(98), Ok("EVEN".to_string()));
+}
+
+#[test]
+pub fn number_err() {
+    assert_eq!(check(37), Err("Provide correct number".to_string()));
+}
+
+#[test]
+pub fn check_ok() {
+    assert_eq!(check(-88), Ok("EVEN".to_string()));
+}
+
+#[test]
+pub fn check_err() {
+    assert_eq!(check(-13), Err("Provide correct number".to_string()));
 }
