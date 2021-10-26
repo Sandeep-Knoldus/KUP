4ec820d913b6ffde91d19ca870424e65c4dcb48d
Sandeep-Knoldus <88487228+Sandeep-Knoldus@users.noreply.github.com>
2021-10-26 22:20:08 +0530

    Update sorting_generic.rs

--- Assignments/generic_traits/src/question1/sorting_generic.rs
+++ Assignments/generic_traits/src/question1/sorting_generic.rs
@@ -8,14 +8,14 @@
 ///
 /// Returns the generic type [T] vector (the sorted list)
 pub fn sorting_numbers<T: PartialOrd>(list: &mut [T]) -> &[T] {
-    for i in 0..list.len() {
-        let mut small = i;
-        for j in (i + 1)..list.len() {
-            if list[j] < list[small] {
-                small = j;
+    for loop1 in 0..list.len() {
+        let mut small = loop1;
+        for loop2 in (loop1 + 1)..list.len() {
+            if list[loop2] < list[small] {
+                small = loop2;
             }
         }
-        list.swap(small, i);
+        list.swap(small, loop1);
     }
     list
 }
