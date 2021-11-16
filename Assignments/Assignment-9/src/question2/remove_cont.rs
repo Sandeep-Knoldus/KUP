94366bec61d7227e71ba422d2ba1507447ecc1d8
Sandeep-Knoldus <88487228+Sandeep-Knoldus@users.noreply.github.com>
2021-11-17 01:19:29 +0530

    Update remove_cont.rs

--- Assignments/Assignment-9/src/question2/remove_cont.rs
+++ Assignments/Assignment-9/src/question2/remove_cont.rs
@@ -8,7 +8,7 @@
 ///
 /// Returns vector Vec<i32> (the new vector without the continuously repeating values)
 pub fn remove_int(vect: Vec<i32>) -> Vec<i32> {
-    let mut vec_2 = Vec::new();
+    let mut vec_2 = vec![..];
     vec_2.push(vect[0]);
     for i in 1..vect.len() {
         if vect[i] != vect[i-1] {
