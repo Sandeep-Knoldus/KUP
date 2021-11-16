e176c20eab3da54462b33eac681ad38f87dc9a28
Sandeep-Knoldus <88487228+Sandeep-Knoldus@users.noreply.github.com>
2021-11-17 01:51:12 +0530

    Update reverse_list.rs

--- Assignments/Assignment-9/src/question2/reverse_list.rs
+++ Assignments/Assignment-9/src/question2/reverse_list.rs
@@ -7,11 +7,11 @@
 /// #Return
 ///
 /// Returns vector Vec<i32> (the new vector with the reversed values)
-pub fn reverse(vect: Vec<i32>) -> Vec<i32>{
+pub fn reverse(vect: Vec<i32>) -> Vec<i32> {
     let mut vect_2 = Vec::new();
     let length = vect.len() - 1;
-    for i in 0..=length {
-        vect_2.push(vect[length - i]);
+    for loop1 in 0..=length {
+        vect_2.push(vect[length - loop1]);
     }
     vect_2
 }
