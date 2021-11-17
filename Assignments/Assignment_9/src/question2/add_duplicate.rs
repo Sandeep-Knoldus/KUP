71cd7bffd9ee436908773ea9165c598dce210129
Sandeep-Knoldus <88487228+Sandeep-Knoldus@users.noreply.github.com>
2021-11-17 18:03:52 +0530

    Update add_duplicate.rs

--- Assignments/Assignment_9/src/question2/add_duplicate.rs
+++ Assignments/Assignment_9/src/question2/add_duplicate.rs
@@ -2,16 +2,16 @@
 ///
 /// #Arguments
 ///
-/// vect: Taking vector Vec<i32> from 'test.rs'
+/// vect: It is a vector Vec<i32>
 ///
 /// #Return
 ///
-/// Returns vector Vec<i32> (the new vector with the duplicate values)
+/// Returns Vec<i32> (the new vector with the duplicate values)
 pub fn duplicate(vect: Vec<i32>) -> Vec<i32> {
     let mut vec_2 = Vec::new();
-    for values in &vect {
+    for values in vect {
         for _loop1 in 0..2 {
-            vec_2.push(*values);
+            vec_2.push(values);
         }
     }
     vec_2
