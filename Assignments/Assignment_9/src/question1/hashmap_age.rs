3a65bc3a9fa043f90365d48cb77d9392308062e4
Sandeep-Knoldus <88487228+Sandeep-Knoldus@users.noreply.github.com>
2021-11-17 18:01:26 +0530

    Update hashmap_age.rs

--- Assignments/Assignment_9/src/question1/hashmap_age.rs
+++ Assignments/Assignment_9/src/question1/hashmap_age.rs
@@ -1,22 +1,22 @@
 pub use std::collections::HashMap;
-/// Function 'sum_conditional' fetches the value of the key
-/// containing the sub-string given and adds them
+/// Function 'sum_conditional' fetches the value of the key, containing the sub-string given and adds them
 ///
 /// #Arguments
 ///
-/// map: Taking Hashmap with name as key with String type
-/// and age as value with i32 type from 'test.rs'
-/// str: Taking String from 'test.rs' for the sub-string
+/// map: Is a Hashmap with name as key and age as value
+/// str: Is a String for the sub-string
 ///
 /// #Return
 ///
-/// Returns integer i32 (the addition of the values)
+/// Returns i32 (the addition of the values)
 pub fn sum_conditional(map: HashMap<String, i32>, str: String) -> i32 {
     let mut result = 0;
     let length = str.len();
     for (key, value) in &map {
-        if key[0..length] == str {
-            result += value;
+        for index in 0..key.len() - length + 1 {
+            if key[index..(index + length)] == str {
+                result += value;
+            }
         }
     }
     result
