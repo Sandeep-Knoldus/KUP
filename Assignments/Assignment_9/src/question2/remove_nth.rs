c14f0ddb48a4ba9ff9c26537cfd2ad06bf6100bb
Sandeep-Knoldus <88487228+Sandeep-Knoldus@users.noreply.github.com>
2021-11-17 18:06:55 +0530

    Update remove_nth.rs

--- Assignments/Assignment_9/src/question2/remove_nth.rs
+++ Assignments/Assignment_9/src/question2/remove_nth.rs
@@ -2,17 +2,17 @@
 ///
 /// #Arguments
 ///
-/// vect: Taking vector Vec<i32> from 'test.rs'
-/// drop_value: Taking i32, as the value to be dropped, from 'test.rs'
+/// vect: It is a vector Vec<i32>
+/// drop_value: It is an integer
 ///
 /// #Return
 ///
-/// Returns vector Vec<i32> (the new vector without the drop_value)
+/// Returns Vec<i32> (the new vector without the drop_value)
 pub fn remove_value(vect: Vec<i32>, drop_value: i32) -> Vec<i32> {
     let mut vec_2 = Vec::new();
-    for values in &vect {
-        if drop_value != *values {
-            vec_2.push(*values);
+    for values in vect {
+        if drop_value != values {
+            vec_2.push(values);
         }
         continue;
     }
