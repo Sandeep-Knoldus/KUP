09c3dd26361a7e4fb0f2c6630c44ed225f4c3115
Sandeep Chakraborty <myselfsandyy@Sandeeps-MacBook-Pro.local>
2021-11-24 22:20:10 +0530

    Added code for feature of async/await

---
+++ Assignments/Assignment-11/src/questions/asynchronous_fashion.rs
@@ -0,0 +1,26 @@
+pub use futures::future::join;
+pub use std::{thread, time};
+
+/// Asynchronous function 'async_fashion' prints data in a Asynchronous Fashion
+///
+/// #Arguments
+///
+/// Nil
+///
+/// #Return
+///
+/// Nil
+pub async fn async_fashion() {
+    log::info!("Data in Asynchronous fashion");
+    let async_fashion = thread::spawn(|| {
+        for i in 1..11 {
+            log::debug!("{}", i);
+            thread::sleep(time::Duration::from_millis(10));
+        }
+    });
+    for i in 1..11 {
+        log::debug!("{}", i);
+        thread::sleep(time::Duration::from_millis(10));
+    }
+    async_fashion.join().unwrap();
+}
---
+++ Assignments/Assignment-11/src/questions/multiplication_table.rs
@@ -0,0 +1,28 @@
+pub use futures::future::join;
+pub use std::{thread, time};
+
+/// Asynchronous function 'async_multiply' prints a table of 2 and 3.
+///
+/// #Arguments
+///
+/// Null
+///
+/// #Return
+///
+/// Null
+pub async fn async_multiply() {
+    log::info!("Multiplication table of 2 and 3");
+    let multiply_by_2 = async {
+        for i in 1..=10 {
+            log::debug!("2 x {} = {}", i, 2 * i);
+            thread::sleep(time::Duration::from_millis(10));
+        }
+    };
+    let multiply_by_3 = async {
+        for i in 1..=10 {
+            log::debug!("3 x {} = {}", i, 3 * i);
+            thread::sleep(time::Duration::from_millis(10));
+        }
+    };
+    join(multiply_by_2, multiply_by_3).await;
+}
