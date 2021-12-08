0e872196d430caaeb4f88f7f4c4692748db1096d
Sandeep-Knoldus <88487228+Sandeep-Knoldus@users.noreply.github.com>
2021-12-08 11:32:24 +0530

    Update main.rs

--- Assignments/task-1/src/main.rs
+++ Assignments/task-1/src/main.rs
@@ -7,21 +7,24 @@ struct Emp {
 
 fn main() {
     let pool = mysql::Pool::new("mysql://root:12345678@localhost:3306/db_test").unwrap();
-    let emps: Vec<Emp> =
-        pool.prep_exec("SELECT emp_id, emp_name from emp_table", ())
-            .map(|result| {
-                result.map(|x| x.unwrap()).map(|row| {
+    let emps: Vec<Emp> = pool
+        .prep_exec("SELECT emp_id, emp_name from emp_table", ())
+        .map(|result| {
+             result
+                .map(|x| x.unwrap())
+                .map(|row| {
                     let (emp_id, emp_name) = mysql::from_row(row);
-                    Emp {
-                        emp_id,
-                        emp_name
-                    }
-                }).collect()
-            }).unwrap();
+                    Emp { emp_id, emp_name }
+                })
+                .collect()
+        })
+        .unwrap();
     for emp in emps {
         println!("{}: {}", emp.emp_id, emp.emp_name);
     }
     pool.prep_exec(
-        r"INSERT INTO emp_table (emp_id, emp_name) VALUES ('3', 'Rohit')",()
-    );
+        r"INSERT INTO emp_table (emp_id, emp_name) VALUES ('3', 'Rohit')",
+        (),
+    )
+    .unwrap();
 }
