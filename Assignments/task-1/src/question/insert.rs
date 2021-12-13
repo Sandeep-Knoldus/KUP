7ef933ae71d75ff39356b6af601dbfb0ef7bea3a
Sandeep Chakraborty <myselfsandyy@Sandeeps-MacBook-Pro.local>
2021-12-13 18:06:13 +0530

    Updated commented changes

--- Assignments/task-1/src/question/insert.rs
+++ Assignments/task-1/src/question/insert.rs
@@ -1,16 +1,16 @@
 pub extern crate mysql;
 
-/// Function 'insert_into_db' inserts values to the table in the database
+/// Function 'insert_into_table_in_database' inserts values in table in the database
 ///
 /// #Arguments
 ///
-/// query_test: Is of type String taking the insert query
-/// link: Is of type String
+/// query_test: Is of type String which takes the INSERT query
+/// link: Is of type String which takes the link to the MySql database
 ///
 /// #Return
 ///
-/// Returns Option<()>
-pub fn insert_into_db(query_test: String, link: String) -> Option<()> {
+/// Returns Option<()> which returns Some() if database and table exists and None if it doesn't
+pub fn insert_into_table_in_database(query_test: String, link: String) -> Option<()> {
     let pool = mysql::Pool::new(link).ok()?;
     pool.prep_exec(query_test, ()).ok()?;
     Some(())
