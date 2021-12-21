41cd65546e71ab5beccf9737278f38bf9f138007
Sandeep-Knoldus <88487228+Sandeep-Knoldus@users.noreply.github.com>
2021-12-21 12:53:26 +0530

    Update test.rs

--- Assignments/task-1/src/test.rs
+++ Assignments/task-1/src/test.rs
@@ -5,65 +5,77 @@ pub mod tests {
     #[test]
     pub fn insert_success() {
         env_logger::init();
-        assert_eq!(
-            insert_into_table_in_database(
-                "INSERT INTO emp_table (emp_name) VALUES ('Rohit')".to_string(),
-                "mysql://root:12345678@localhost:3306/db_test".to_string()
-            ),
-            None
+        assert!(
+            matches!(
+                insert_into_table_in_database(
+                    "INSERT INTO emp (emp_name) VALUES ('Sandy')".to_string(),
+                    "mysql://root:12345678@localhost:3306/db_test".to_string()
+                ).as_deref(),
+                Ok("Connection Established")
+            )
         );
     }
     #[test]
-    pub fn insert_failure() {
-        assert_eq!(
-            insert_into_table_in_database(
-                "INSERT INTO emp_table (emp_name) VALUES ('Rohit')".to_string(),
-                "mysql://root:12345678@localhost:3306/db".to_string() // No such database named 'db'
-            ),
-            None
+    pub fn insert_db_failure() {
+        assert!(
+            matches!(
+                insert_into_table_in_database(
+                    "INSERT INTO emp (emp_name) VALUES ('Rohit')".to_string(),
+                    "mysql://root:12345678@localhost:3306/db".to_string() // No such database named 'db'
+                ),
+                Err(_)
+            )
         );
         log::error!("No such database named 'db'");
     }
     #[test]
-    pub fn insert_fail() {
-        assert_eq!(
-            insert_into_table_in_database(
-                "INSERT INTO emp (emp_name) VALUES ('Rohit')".to_string(), // No such table named 'emp'
-                "mysql://root:12345678@localhost:3306/db_test".to_string()
-            ),
-            None
+    pub fn insert_table_failure() {
+        assert!(
+            matches!(
+                insert_into_table_in_database(
+                    "INSERT INTO emp_tab (emp_name) VALUES ('Rohit')".to_string(), // No such table named 'emp'
+                    "mysql://root:12345678@localhost:3306/db_test".to_string()
+                ),
+                Err(_)
+            )
         );
         log::error!("No such table named 'emp'");
     }
     #[test]
     pub fn view_db_table_success() {
-        assert_eq!(
-            view_table_in_database(
-                "SELECT emp_id, emp_name from emp_table".to_string(),
-                "mysql://root:12345678@localhost:3306/db_test".to_string()
-            ),
-            None
+        assert!(
+            matches!(
+                view_table_in_database(
+                    "SELECT emp_id, emp_name from emp".to_string(),
+                    "mysql://root:12345678@localhost:3306/db_test".to_string()
+                ).as_deref(),
+                Ok("Connection Established")
+            )
         );
     }
     #[test]
-    pub fn view_db_table_failure() {
-        assert_eq!(
-            view_table_in_database(
-                "SELECT emp_id, emp_name from emp_table".to_string(),
-                "mysql://root:12345678@localhost:3306/db".to_string() // No such database named 'db'
-            ),
-            None
+    pub fn view_no_db_failure() {
+        assert!(
+            matches!(
+                view_table_in_database(
+                    "SELECT emp_id, emp_name from emp".to_string(),
+                    "mysql://root:12345678@localhost:3306/db".to_string() // No such database named 'db'
+                ),
+                Err(_)
+            )
         );
         log::error!("No such database named 'db'");
     }
     #[test]
-    pub fn view_db_table_fail() {
-        assert_eq!(
-            view_table_in_database(
-                "SELECT emp_id, emp_name from emp".to_string(), // No such table named 'emp'
-                "mysql://root:12345678@localhost:3306/db_test".to_string()
-            ),
-            None
+    pub fn view_no_table_failure() {
+        assert!(
+            matches!(
+                view_table_in_database(
+                    "SELECT emp_id, emp_name from emp_tab".to_string(), // No such table named 'emp'
+                    "mysql://root:12345678@localhost:3306/db_test".to_string()
+                ),
+                Err(_)
+            )
         );
         log::error!("No such table named 'emp'");
     }
