0334a8b55c48a2b65ec4a8bc03ed1f36688b7479
Sandeep-Knoldus <88487228+Sandeep-Knoldus@users.noreply.github.com>
2021-12-22 14:25:30 +0530

    Update info.rs

--- Assignments/post_rust_lib/src/info.rs
+++ Assignments/post_rust_lib/src/info.rs
@@ -9,5 +9,5 @@
 /// Returns String
 pub async fn add_user() -> String {
     log::info!("Calling the 'add_user' function");
-    format!("This is called from the add_user")
+    "This is called from the add_user".to_string()
 }
