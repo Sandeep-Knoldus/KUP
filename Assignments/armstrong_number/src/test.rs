42d3de648b72832509d5112948ff6778ecc47e3e
Sandeep-Knoldus <88487228+Sandeep-Knoldus@users.noreply.github.com>
2022-01-06 13:19:06 +0530

    Update test.rs

--- Assignments/armstrong_number/src/test.rs
+++ Assignments/armstrong_number/src/test.rs
@@ -39,15 +39,35 @@ pub mod tests {
         assert_eq!(palindrome_seq(123412), false)
     }
     #[test]
-    pub fn matrix_success() {
-        let mat_1 = [[1, 2], [3, 4]];
-        let mat_2 = [[5, 6], [7, 8]];
-        assert_eq!(matrix_a_b(mat_1, mat_2), [[19, 22], [43, 50]]);
+    fn multiplication_first_success() {
+        let first = [[1, 2, 3].to_vec(), [4, 5, 6].to_vec(), [7, 8, 9].to_vec()].to_vec();
+        let second = [[1, 0, 0].to_vec(), [0, 1, 0].to_vec(), [0, 0, 1].to_vec()].to_vec();
+        let right = [[1, 2, 3].to_vec(), [4, 5, 6].to_vec(), [7, 8, 9].to_vec()].to_vec();
+        assert_eq!(matrix_a_b(&first, &second), Ok(right));
     }
     #[test]
-    pub fn matrix_failure() {
-        let mat_1 = [[3, 1], [8, 9]];
-        let mat_2 = [[5, 6], [7, 8]];
-        assert_ne!(matrix_a_b(mat_1, mat_2), [[19, 22], [43, 50]]);
+    fn multiplication_second_success() {
+        let first = [[1, 2].to_vec(), [3, 4].to_vec()].to_vec();
+        let second = [[1, 0].to_vec(), [0, 1].to_vec()].to_vec();
+        let right = [[1, 2].to_vec(), [3, 4].to_vec()].to_vec();
+        assert_eq!(matrix_a_b(&first, &second), Ok(right));
+    }
+    #[test]
+    fn multiplication_first_failure() {
+        let first = [[1, 2].to_vec(), [3, 4].to_vec()].to_vec();
+        let second = [[1, 2, 3].to_vec(), [4, 5, 6].to_vec(), [7, 8, 9].to_vec()].to_vec();
+        assert_eq!(
+            matrix_a_b(&first, &second),
+            Err("Not possible".to_string())
+        );
+    }
+    #[test]
+    fn multiplication_second_failure() {
+        let first = [[1, 2, 3].to_vec(), [4, 5, 6].to_vec(), [7, 8, 9].to_vec()].to_vec();
+        let second = [[1, 2].to_vec(), [3, 4].to_vec()].to_vec();
+        assert_eq!(
+            matrix_a_b(&first, &second),
+            Err("Not possible".to_string())
+        );
     }
 }
