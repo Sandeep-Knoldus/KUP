f73207456afcd1e85010c2f6294372e3d48f7cda
Sandeep Chakraborty <myselfsandyy@Sandeeps-MacBook-Pro.local>
2022-01-06 12:30:42 +0530

    Updated matrix multiplication

--- Assignments/armstrong_number/src/modules/matrix_multiply.rs
+++ Assignments/armstrong_number/src/modules/matrix_multiply.rs
@@ -4,20 +4,20 @@ pub use crate::operations::{op, operations_main};
 ///
 /// #Arguments
 ///
-/// mat_1: It is of type Vec<Vec<i32>> taking the first matrix
-/// mat_2: It is of type Vec<Vec<i32>> taking the second matrix
+/// arr1: It is of type [[usize; 2]; 2] taking the first matrix
+/// arr2: It is of type [[usize; 2]; 2] taking the second matrix
 ///
 /// #Return
 ///
-/// Returns [[i32; 3]; 3]
-pub fn matrix_a_b(mat_1: Vec<Vec<i32>>, mat_2: Vec<Vec<i32>>) -> [[i32; 3]; 3] {
-    let mut new_a: [[i32; 3]; 3] = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
-    for loop1 in 0..3{
-        for loop2 in 0..3{
-            for loop3 in 0..3{
-                new_a[loop1][loop2] += mat_1[loop1][loop3] * mat_2[loop3][loop2];
+/// Returns [[i32; 2]; 2] the multiplied matrix
+pub fn matrix_a_b(arr1: [[usize; 2]; 2], arr2: [[usize; 2]; 2]) -> [[usize; 2]; 2] {
+    let mut new_arr: [[usize; 2]; 2] = [[0_usize; 2]; 2];
+    for loop1 in 0..2 {
+        for loop2 in 0..2 {
+            for (loop3, _i) in arr2.iter().enumerate().take(2) {
+                new_arr[loop1][loop2] += arr1[loop1][loop3] * arr2[loop3][loop2];
             }
         }
     }
-    return new_a;
+    new_arr
 }
