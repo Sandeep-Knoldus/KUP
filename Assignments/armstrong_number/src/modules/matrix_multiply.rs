4d31783507d9cb3d79e72932d58d56da23218df6
Sandeep-Knoldus <88487228+Sandeep-Knoldus@users.noreply.github.com>
2022-01-06 13:19:42 +0530

    Update matrix_multiply.rs

--- Assignments/armstrong_number/src/modules/matrix_multiply.rs
+++ Assignments/armstrong_number/src/modules/matrix_multiply.rs
@@ -4,20 +4,33 @@ pub use crate::operations::{op, operations_main};
 ///
 /// #Arguments
 ///
-/// arr1: It is of type [[usize; 2]; 2] taking the first matrix
-/// arr2: It is of type [[usize; 2]; 2] taking the second matrix
+/// first_matrix: It is of type &[Vec<i32>] taking the first matrix
+/// second_matrix: It is of type &[Vec<i32>] taking the second matrix
 ///
 /// #Return
 ///
-/// Returns [[i32; 2]; 2] the multiplied matrix
-pub fn matrix_a_b(arr1: [[usize; 2]; 2], arr2: [[usize; 2]; 2]) -> [[usize; 2]; 2] {
-    let mut new_arr: [[usize; 2]; 2] = [[0_usize; 2]; 2];
-    for loop1 in 0..2 {
-        for loop2 in 0..2 {
-            for (loop3, _i) in arr2.iter().enumerate().take(2) {
-                new_arr[loop1][loop2] += arr1[loop1][loop3] * arr2[loop3][loop2];
-            }
-        }
+/// Returns Result<Vec<Vec<i32>>, String>
+pub fn matrix_a_b(
+    first_matrix: &[Vec<i32>],
+    second_matrix: &[Vec<i32>],
+) -> Result<Vec<Vec<i32>>, String> {
+    if first_matrix[0].len() != second_matrix.len() {
+        return Err("Not possible".to_string());
     }
-    new_arr
+    let mut result: Vec<Vec<i32>> = Vec::new();
+    let l_1 = first_matrix.len();
+    let l_2 = second_matrix.len();
+    for (index_1, _item) in first_matrix.iter().enumerate().take(l_1)  {
+        log::info!("{}",index_1);
+        let mut vec: Vec<i32> = Vec::new();
+        for index_2 in 0..second_matrix[0].len() {
+            let mut sum = 0;
+            for (index_3, _index) in second_matrix.iter().enumerate().take(l_2){
+                sum += first_matrix[index_1][index_3] * second_matrix[index_3][index_2];
+            }
+            vec.push(sum);
+        }
+        result.push(vec);
+    }
+    Ok(result)
 }
